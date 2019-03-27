070ec8f25e3a57618fe51109288d9050e2cf03f5
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 17:08:33 -0400

    Remove frivolous mapping

--- frontend/components/nav/nav_controls/nav_controls_container.jsx
+++ frontend/components/nav/nav_controls/nav_controls_container.jsx
@@ -1,6 +1,5 @@
 import { connect } from "react-redux";
 import NavControls from './nav_controls';
-import { logout } from "../../../actions/session/session_actions";
 import { withRouter } from 'react-router-dom';
 
 const msp = (state) => ({
@@ -8,7 +7,7 @@ const msp = (state) => ({
 });
 
 const mdp = (dispatch) => ({
-    logout: () => dispatch( logout() )
+
 });
 
 export default withRouter(connect(msp, mdp)(NavControls));
