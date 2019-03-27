b80e58cc967c32fdf16e3ac2d6a75b7110fe0c71
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-27 12:19:39 -0400

    Set preloaded state according to current user

--- frontend/eulr.jsx
+++ frontend/eulr.jsx
@@ -4,7 +4,23 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 const cb = () => {
-    const store = configureStore();
+
+    let preloadedState = {};
+    if (window.currentUser) {
+        preloadedState = {
+            session: { 
+                id: window.currentUser.id 
+            },
+            entities: {
+                users: {
+                    [window.currentUser.id]: window.currentUser
+                }
+            }
+        };
+    }
+    const store = configureStore(preloadedState);
+    delete window.currentUser;
+
     const root = document.getElementById("root");
 
     ReactDOM.render(<Root store={store} />, root);
