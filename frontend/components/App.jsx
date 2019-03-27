2f2af768708b0b4bd8338284232dc258dd48744e
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 14:05:56 -0400

    Render navbar

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,10 +9,11 @@ import {
 import Splash from './splash/splash_container';
 import LoginForm from './splash/login/login_form_container';
 import Explore from './dash/explore/explore_container';
+import Navbar from './nav/nav_container';
 
 const App = (props) => (
     <div className="app-container">
-        <h1>Welcome to Euler!</h1>
+        <Navbar />
         <Switch>
             <Route path="/explore" component={Explore} />
             <Route path="/login" component={LoginForm} />
