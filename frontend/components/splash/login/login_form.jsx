cd45ebd91035186da0171d408a69668f6b230d8a
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 14:20:32 -0400

    Conditionally render email or password input

--- frontend/components/splash/login/login_form.jsx
+++ frontend/components/splash/login/login_form.jsx
@@ -6,12 +6,28 @@ class LoginForm extends React.Component {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
-        this.state = props.user;
+        this.handleNext = this.handleNext.bind(this);
+        this.state = {
+            email: "",
+            password: "",
+            stage: 1
+        };
+    }
+
+    handleNext(event) {
+        event.preventDefault();
+        this.setState({
+            stage: 2
+        });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.props.loginUser(this.state);
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        this.props.login(user);
     }
 
     handleInput(type) {
@@ -23,26 +39,37 @@ class LoginForm extends React.Component {
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleInput("email")}
-                ></input>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleInput("password")}
-                ></input>
-                <input
-                    type="submit"
-                    value="Log In"
-                ></input>
-            </form>
-        );
+        if (this.state.stage === 1) {
+            return (
+                <form onSubmit={this.handleNext}>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={this.state.email}
+                        onChange={this.handleInput("email")}
+                    ></input>
+                    <input
+                        type="submit"
+                        value="Next"
+                    ></input>
+                </form>
+            );
+        } else if (this.state.stage === 2) {
+            return (
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.password}
+                        onChange={this.handleInput("password")}
+                    ></input>
+                    <input
+                        type="submit"
+                        value="Log In"
+                    ></input>
+                </form>
+            );
+        }
     }
 }
 
