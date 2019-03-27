f31df73455d43062501e3177f4d115558e88822f
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 17:25:29 -0400

    Add demo login

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,6 +3,22 @@ import { Link } from 'react-router-dom';
 import RegisterForm from './register/register_form_container';
 
 class Splash extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDemoLogin = this.handleDemoLogin.bind(this);
+    }
+
+    handleDemoLogin() {
+        const demoUser = {
+            email: "leuler@mathmail.com",
+            password: "123456"
+        };
+        this.props.demoLogin(demoUser).then(
+            this.props.history.push("/dashboard")
+        );
+    }
+
     render() {
         return(
             <div className="splash-container">
@@ -10,6 +26,7 @@ class Splash extends React.Component {
             <p>An infinite series of possibilities converges here.</p>
             <RegisterForm className="register-form"/>
             <Link to="/login">Log In</Link>
+            <button onClick={this.handleDemoLogin}>Demo Login</button>
             <Link to="/explore">Here's what's trending</Link>
             </div>
         );
--- frontend/components/splash/splash_container.jsx
+++ frontend/components/splash/splash_container.jsx
@@ -1,12 +1,14 @@
 import Splash from './splash';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { login } from '../../actions/session/session_actions';
 
 const msp = (state) => ({
 
 });
 
 const mdp = (dispatch) => ({
-
+    demoLogin: (user) => dispatch( login(user) )
 });
 
-export default connect(msp, mdp)(Splash);
+export default withRouter(connect(msp, mdp)(Splash));
