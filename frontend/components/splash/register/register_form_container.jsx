d6f2d82c679cd4983afec3b65f3b289005bc5f6e
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 14:47:51 -0400

    Pass login dispatch to connected component

--- frontend/components/splash/register/register_form_container.jsx
+++ frontend/components/splash/register/register_form_container.jsx
@@ -1,13 +1,15 @@
 import RegisterForm from './register_form';
 import { connect } from 'react-redux';
 import { createUser } from '../../../actions/entities/user_actions';
+import { login } from '../../../actions/session/session_actions';
 
 const msp = (state) => ({
 
 });
 
 const mdp = (dispatch) => ({
-    createUser: (user) => dispatch( createUser(user) )
+    createUser: (user) => dispatch( createUser(user) ),
+    login: (user) => dispatch( login(user) )
 });
 
 export default connect(msp, mdp)(RegisterForm);
