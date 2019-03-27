8516a521beb4305b2ed537367622444e02f48bd7
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 14:34:08 -0400

    Initial commit

---
+++ frontend/components/dash/dash.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Dash extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Coming soon!</h2>
+            </div>
+        );
+    }
+}
+
+export default Dash;
---
+++ frontend/components/dash/dash_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import Dash from './dash';
+
+const msp = (state) => ({
+
+});
+
+const mdp = (dispatch) => ({
+
+});
+
+export default connect(msp, mdp)(Dash);
