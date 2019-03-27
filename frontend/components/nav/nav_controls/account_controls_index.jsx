94627ffbf1caeb4e545655c92e0780daa1a2de52
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 16:56:29 -0400

    Initial commit

---
+++ frontend/components/nav/nav_controls/account_controls_index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class NavControls extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        this.props.logout().then(() => {
+            this.props.history.push("/");
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h5>Account</h5>
+                <button onClick={this.handleLogout}>Log Out</button>
+                <ul>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default NavControls;
---
+++ frontend/components/nav/nav_controls/account_controls_index_container.jsx
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import AccountControls from './nav_controls';
+import { logout } from "../../../actions/session/session_actions";
+import { withRouter } from 'react-router-dom';
+
+const msp = (state) => ({
+
+});
+
+const mdp = (dispatch) => ({
+    logout: () => dispatch(logout())
+});
+
+export default withRouter(connect(msp, mdp)(AccountControls));
