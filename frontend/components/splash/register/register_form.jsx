7b2ea174a077e49844d47e8cbfedba2cdb42b40f
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 14:43:19 -0400

    Redirect to dashboard on sucessful registration

--- frontend/components/splash/register/register_form.jsx
+++ frontend/components/splash/register/register_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { merge } from 'lodash';
 
 class RegisterForm extends React.Component {
@@ -8,10 +9,23 @@ class RegisterForm extends React.Component {
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.handleOpen = this.handleOpen.bind(this);
+        this.buildUser = this.buildUser.bind(this);
         this.state = {
-            user: props.user,
-            open: false
+            email: "",
+            username: "",
+            password: "",
+            open: false,
+            redirect: false
         };
+    }
+
+    buildUser() {
+        const user = {
+            email: this.state.email,
+            username: this.state.username,
+            password: this.state.password
+        };
+        return user;
     }
 
     handleOpen() {
@@ -22,48 +36,54 @@ class RegisterForm extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        const user = this.state.user;
+        const user = this.buildUser();
         this.props.createUser(user);
+        this.setState({
+            redirect: true
+        });
     }
 
     handleInput(type) {
         return (event) => {
-            const user = merge({}, this.state.user);
-            user[type] = event.target.value;
             this.setState({
-                user: user
+                [type]: event.target.value
             });
         };
     }
 
     render() {
-        if (this.state.open) {
-        return(
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="Email"
-                        value={this.state.user.email}
-                        onChange={this.handleInput("email")}
-                    ></input>
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={this.state.user.password}
-                        onChange={this.handleInput("password")}
-                    ></input>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={this.state.user.username}
-                        onChange={this.handleInput("username")}
-                    ></input>
-                    <input
-                        type="submit"
-                        value="Sign Up"
-                    ></input>
-                </form>
+        if (this.state.redirect) {
+            return (
+                <Redirect to="/dashboard" />
             );
+        }
+        if (this.state.open) {
+            return (
+                    <form onSubmit={this.handleSubmit}>
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            value={this.state.email}
+                            onChange={this.handleInput("email")}
+                        ></input>
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handleInput("password")}
+                        ></input>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={this.state.username}
+                            onChange={this.handleInput("username")}
+                        ></input>
+                        <input
+                            type="submit"
+                            value="Sign Up"
+                        ></input>
+                    </form>
+                );
         } else {
             return (
                 <button onClick={this.handleOpen}>Get Started</button>
