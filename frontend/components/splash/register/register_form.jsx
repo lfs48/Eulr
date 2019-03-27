dc017f1eba068c5ee9d4e1249ab84cb80224d1f6
App Academy Student <aa@Apps-Mac-mini.local>
2019-03-27 11:57:14 -0400

    Make form initially closed, with clickable button to set it to open state and display form

--- frontend/components/splash/register/register_form.jsx
+++ frontend/components/splash/register/register_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { merge } from 'lodash';
 
 class RegisterForm extends React.Component {
     
@@ -6,49 +7,68 @@ class RegisterForm extends React.Component {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
-        this.state = props.user;
+        this.handleOpen = this.handleOpen.bind(this);
+        this.state = {
+            user: props.user,
+            open: false
+        };
+    }
+
+    handleOpen() {
+        this.setState({
+            open: true
+        });
     }
 
     handleSubmit(event) {
         event.preventDefault();
-        this.props.createUser(this.state);
+        const user = this.state.user;
+        this.props.createUser(user);
     }
 
     handleInput(type) {
         return (event) => {
+            const user = merge({}, this.state.user);
+            user[type] = event.target.value;
             this.setState({
-                [type]: event.target.value
+                user: user
             });
         };
     }
 
     render() {
+        if (this.state.open) {
         return(
-            <form onSubmit={this.handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Email"
-                    value={this.state.email}
-                    onChange={this.handleInput("email")}
-                ></input>
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={this.state.password}
-                    onChange={this.handleInput("password")}
-                ></input>
-                <input
-                    type="text"
-                    placeholder="Username"
-                    value={this.state.username}
-                    onChange={this.handleInput("username")}
-                ></input>
-                <input
-                    type="submit"
-                    value="Sign Up"
-                ></input>
-            </form>
-        );
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Email"
+                        value={this.state.user.email}
+                        onChange={this.handleInput("email")}
+                    ></input>
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={this.state.user.password}
+                        onChange={this.handleInput("password")}
+                    ></input>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={this.state.user.username}
+                        onChange={this.handleInput("username")}
+                    ></input>
+                    <input
+                        type="submit"
+                        value="Sign Up"
+                    ></input>
+                </form>
+            );
+        } else {
+            return (
+                <button onClick={this.handleOpen}>Get Started</button>
+            );
+        }
     }
 }
 
