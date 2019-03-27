7eb910096de95a96c55d9eba58f762acfc67df09
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-27 16:29:12 -0400

    Render nav controls when logged in

--- frontend/components/nav/nav.jsx
+++ frontend/components/nav/nav.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import Search from './search/search_container';
+import NavControls from './nav_controls/nav_controls_container';
 
 const Navbar = (props) => {
-    return(
-        <div className="eulr-nav">
-            <Search />
-            <h1>Welcome to Euler{props.loggedIn ? `, esteemed user` : '!'}</h1>
-        </div>
-    );
+    if (props.loggedIn) {
+        return(
+            <div className="eulr-nav">
+                <Search />
+                <h1>Welcome to Euler, esteemed user!</h1>
+                <NavControls />
+            </div>
+        );
+    } else {
+        return(
+            <div className="eulr-nav">
+                <Search />
+                <h1>Welcome to Euler!</h1>
+            </div>
+        );
+    }
 };
 
 export default Navbar;
