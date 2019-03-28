4653b89a810f7b3611a626f9affbc0740602f494
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-28 17:02:08 -0400

    Handle RECEIVE_LOGIN_ERRORS and RECEIVE_REGISTER_ERRORS

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,11 +1,19 @@
 import { merge } from 'lodash';
+import { RECEIVE_SIGNUP_ERRORS } from '../../actions/entities/user_actions';
+import { RECEIVE_LOGIN_ERRORS } from '../../actions/session/session_actions';
 
 const sessionErrorsReducer = (state = {}, action) => {
+    debugger
     const newState = merge({}, state);
     Object.freeze(state);
 
     switch(action.type) {
         default: return state;
+
+        case (RECEIVE_LOGIN_ERRORS):
+        case (RECEIVE_SIGNUP_ERRORS): {
+            return action.errors;
+        }
     }
 };
 
