ba19d21dedc005bfce0123cd0f49e967224dbe3b
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-28 13:45:35 -0400

    Initial commit

--- frontend/components/nav/nav_controls/account_controls_index.jsx
+++ frontend/components/nav/nav_controls/account_controls_index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class AccountControls extends React.Component {
 
@@ -27,20 +28,38 @@ class AccountControls extends React.Component {
         if (this.state.open) {
             return (
                 <div>
-                    <button onClick={this.handleToggle}>
+                    <button className="account-menu-icon" onClick={this.handleToggle}>
                         <i className="fas fa-user"></i>
                     </button>
                     <div className="account-menu">
-                        <h5>Account</h5>
-                        <button onClick={this.handleLogout}>Log Out</button>
+                        <header className="account-menu-header">
+                            <h5>Account</h5>
+                            <button onClick={this.handleLogout}>Log Out</button>
+                        </header>
                         <ul>
+                            <li>
+                                <i className="fas fa-heart"></i>
+                                <Link to="/likes">Likes</Link>
+                            </li>
+                            <li>
+                                <i className="fas fa-user-plus"></i>
+                                <Link to="/following">Following</Link>
+                            </li>
+                            <li>
+                                <i className="fas fa-cog"></i>
+                                <Link to="/settings">Settings</Link>
+                            </li>
+                            <li>
+                                <i className="fas fa-question-circle"></i>
+                                <Link to="help">Help</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
             );
         } else {
             return (
-                <button onClick={this.handleToggle}>
+                <button className="account-menu-icon" onClick={this.handleToggle}>
                     <i className="fas fa-user"></i>
                 </button>
             );
---
+++ frontend/components/under_construction/under_construction.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h1>This page is currently under construction, check again later!</h1>
