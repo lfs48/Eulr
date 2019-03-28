ce9bfc8bf81764a854f27f71e15d43185b67d720
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-28 16:14:23 -0400

    Uncomment

--- frontend/reducers/errors/errors_reducer.js
+++ frontend/reducers/errors/errors_reducer.js
@@ -1,7 +1,9 @@
-// import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
+import sessionErrorsReducer from './session_errors_reducer';
 
-// const errorsReducer = combineReducers({
 
-// });
+const errorsReducer = combineReducers({
+    session: sessionErrorsReducer
+});
 
-// export default errorsReducer;
+export default errorsReducer;
