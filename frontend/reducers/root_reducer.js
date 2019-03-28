5948058ae1d24fd9cbf82777588ccf935786e75d
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-28 16:14:05 -0400

    Incorporate errors reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -7,6 +7,7 @@ import errorsReducer from './errors/errors_reducer';
 const rootReducer = combineReducers({
     entities: entitiesReducer,
     session: sessionReducer,
+    errors: errorsReducer
 });
 
 export default rootReducer;
