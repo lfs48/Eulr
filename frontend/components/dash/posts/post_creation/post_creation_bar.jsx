89cb62d56fa5de50cd935b3b0c82d219c12e7984
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 14:13:06 -0400

    Pass postType prop to list items

--- frontend/components/dash/posts/post_creation/post_creation_bar.jsx
+++ frontend/components/dash/posts/post_creation/post_creation_bar.jsx
@@ -3,18 +3,18 @@ import PostCreationBarItem from './post_creation_bar_item_container';
 
 const PostCreationBar = (props) => {
     const iconLabels = [
-        ["fas fa-font", "Text"],
-        ["fas fa-camera", "Photo"],
-        ["fas fa-quote-left", "Quote"],
-        ["fas fa-link", "Link"],
-        ["fas fa-comment-alt", "Chat"],
-        ["fas fa-headphones", "Audio"],
-        ["fas fa-video", "Video"]
+        ["fas fa-font", "Text", "text"],
+        ["fas fa-camera", "Photo", "photo"],
+        ["fas fa-quote-left", "Quote", "quote"],
+        ["fas fa-link", "Link", "link"],
+        ["fas fa-comment-alt", "Chat", "link"],
+        ["fas fa-headphones", "Audio", "audio"],
+        ["fas fa-video", "Video", "video"]
     ];
     const lis = iconLabels.map( (el, idx) => {
         return (
                 <li key={idx}>
-                    <PostCreationBarItem icon={el[0]} label={el[1]} />
+                    <PostCreationBarItem icon={el[0]} label={el[1]} postType={el[2]} />
                 </li>
             );
     })
