6e840dcb2aef2c0306df27db40a411293be3a522
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 11:39:21 -0400

    Initial commit

---
+++ frontend/components/dash/posts/post_index/post_creation/post_creation_bar_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import PostCreationBar from './posts_creation_bar';
+
+const msp = () => ({
+
+});
+
+const mdp = () => ({
+
+});
+
+export default connect(msp, mdp)(PostCreationBar);
