17aa3504f12a8608176448a253b05899957bc228
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 10:41:38 -0400

    Create additional posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :create, :show]
+    resources :users, only: [:index, :create, :show] do
+        resources :posts, only: [:index]
+      end
     resource :session, only: [:create, :destroy]
-    resources :posts, only: [:index, :create, :update, :destroy]
+    resources :posts, only: [:index, :create, :update, :show, :destroy]
   end
   
 end
