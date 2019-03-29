562211c579a5669c07f6ca4bd459625f441704cc
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 11:58:06 -0400

    Render post creation bar

--- frontend/components/dash/dash.jsx
+++ frontend/components/dash/dash.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PostCreationBar from './posts/post_creation/post_creation_bar_container';
 
 class Dash extends React.Component {
     render() {
         return (
             <div>
-                <h2>Coming soon!</h2>
+                <PostCreationBar />
             </div>
         );
     }
