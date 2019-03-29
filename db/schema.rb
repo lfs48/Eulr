4b6199f19e2d0d08fe4ac5ae0cbc30b0e45d3723
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 10:35:08 -0400

    Migrate edit to posts table ranming type to post_type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_140011) do
+ActiveRecord::Schema.define(version: 2019_03_29_142514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2019_03_29_140011) do
     t.integer "author_id", null: false
     t.integer "poster_id", null: false
     t.text "content", null: false
-    t.string "type", null: false
+    t.string "post_type", null: false
     t.integer "parent_post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
