5476db0e27217aefa0d99606fb47ba198d5f25b0
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 11:33:04 -0400

    Handle RECEIVE_POST, RECEIVE_ALL_POSTS, REMOVE_POST actions

--- frontend/reducers/entities/posts_reducer.js
+++ frontend/reducers/entities/posts_reducer.js
@@ -1,4 +1,5 @@
 import { merge } from 'lodash';
+import { RECEIVE_ALL_POSTS, RECEIVE_POST, REMOVE_POST } from '../../actions/entities/post_actions';
 
 const postsReducer = (state = {}, action) => {
     const newState = merge({}, state);
@@ -7,6 +8,21 @@ const postsReducer = (state = {}, action) => {
     switch(action.type) {
 
         default: return state;
+
+        case(RECEIVE_ALL_POSTS): {
+            return action.posts;
+        }
+
+        case(RECEIVE_POST): {
+            const post = action.post;
+            newState[post.id] = post;
+            return newState;
+        }
+
+        case(REMOVE_POST): {
+            delete newState[action.id];
+            return newState;
+        }
     }
 };
 
