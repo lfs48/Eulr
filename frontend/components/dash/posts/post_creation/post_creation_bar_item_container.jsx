337b4a5f85dfcd191350e1ca4dc2f21c7c77c670
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 12:00:29 -0400

    Create skeleton

---
+++ frontend/components/dash/posts/post_creation/post_creation_bar_item_container.jsx
@@ -0,0 +1,12 @@
+import { connect } from "react-redux";
+import PostCreationBarItem from './post_creation_bar_item';
+
+const msp = () => ({
+
+});
+
+const mdp = () => ({
+
+});
+
+export default connect(msp, mdp)(PostCreationBarItem);
