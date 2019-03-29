95a1a8bece09789655374eba4a5de95b9388792b
Lucas Schraier <lucasfschraier@gmail.com>
2019-03-29 14:57:14 -0400

    Dispatch createPost action on submission of post

--- frontend/components/dash/posts/post_creation/post_forms/text_post_form.jsx
+++ frontend/components/dash/posts/post_creation/post_forms/text_post_form.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { createPost } from '../../../../../actions/entities/post_actions';
+import { merge } from 'lodash';
 
 class PostForm extends React.Component {
     
@@ -7,40 +9,54 @@ class PostForm extends React.Component {
         super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleInput = this.handleInput.bind(this);
-        this.state = props.post;
+        this.state = {
+            post: props.post,
+            content: props.content
+        };
     }
 
     handleSubmit(event) {
         event.preventDefault();
+        const content = JSON.stringify(this.state.content);
+        const post = merge({}, this.state.post);
+        post.content = content;
+        this.props.createPost(post);
     }
 
     handleInput(type) {
         return (event) => {
+            const content = merge({}, this.state.content);
+            content[type] = event.target.value;
             this.setState({
-                [type]: event.target.value
+                content: content
             });
         };
     }
 
     render() {
+        let disabled = false;
+        if (this.state.content.title === "" && this.state.content.body === "") {
+            disabled = true;
+        }
         return (
             <div className="post-form-container">
                 <form onSubmit={this.handleSubmit}>
                     <input
                         type="text"
                         placeholder="Title"
-                        value={this.state.title}
+                        value={this.state.content.title}
                         onChange={this.handleInput("title")}
                     ></input>
                     <input
                         type="text"
                         placeholder="Your text here"
-                        value={this.state.content}
-                        onChange={this.handleInput("content")}
+                        value={this.state.content.body}
+                        onChange={this.handleInput("body")}
                     ></input>
                     <input
                         type="submit"
                         value="Post"
+                        disabled={disabled}
                     ></input>
                 </form>
             </div>
@@ -50,8 +66,13 @@ class PostForm extends React.Component {
 
 const msp = (state) => ({
     post: {
+        author_id: state.session.id,
+        poster_id: state.session.id,
+        post_type: "text"
+    },
+    content: {
         title: "",
-        content: ""
+        body: ""
     }
 });
 
