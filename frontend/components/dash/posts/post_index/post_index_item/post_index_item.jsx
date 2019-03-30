56b1350e41f01b6aac74dc6cab295a0ef69193dd
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 16:27:05 -0400

    Initial commit

---
+++ frontend/components/dash/posts/post_index/post_index_item/post_index_item.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class PostIndexItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleEdit() {
+
+    }
+
+    handleDelete() {
+        this.props.deletePost(this.props.post.id);
+    }
+
+    componentDidMount() {
+        this.props.author ? null : this.props.fetchUser(this.props.post.author_id);
+    }
+
+    render() {
+        return(
+            <div>
+                <h4>{this.props.author ? this.props.author.username : ""}</h4>
+                <span>{this.props.post.content}</span>
+                <button onClick={this.handleEdit}>Edit</button>
+                <button onClick={this.handleDelete}>Delete</button>
+            </div>
+        )
+    }
+}
+
+export default PostIndexItem;
