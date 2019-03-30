a7c2e75050287225f31e729730d05784619e2e56
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 18:05:17 -0400

    Add css classes to elements

--- frontend/components/nav/search/search.jsx
+++ frontend/components/nav/search/search.jsx
@@ -14,15 +14,22 @@ class Search extends React.Component {
             search: event.target.value
         });
     }
+
+    handleClick(event) {
+        event.currentTarget.className = "search-container-clicked"
+    }
+
     render() {
         return(
-            <input
-                className="nav-search"
+            <div className="search-container" onClick={this.handleClick}>
+            <i className="fas fa-search"></i>
+            <input className="nav-search"
                 type="text"
                 placeholder="Search Eulr"
                 value={this.state.search}
                 onChange={this.handleInput}
             ></input>
+            </div>
         );
     }
 }
