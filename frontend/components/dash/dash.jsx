e9f298768869b6622f339cdef58779485ad823f2
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 11:40:26 -0400

    Render post index

--- frontend/components/dash/dash.jsx
+++ frontend/components/dash/dash.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PostCreationBar from './posts/post_creation/post_creation_bar_container';
+import PostIndex from './posts/post_index/post_index_container';
 
 class Dash extends React.Component {
     render() {
         return (
             <div>
                 <PostCreationBar />
+                <PostIndex />
             </div>
         );
     }
