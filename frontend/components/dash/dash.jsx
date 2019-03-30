1033e7ea782833a5ac206587a438f209a94b0205
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 12:27:00 -0400

    Define /new routes to open post creation forms

--- frontend/components/dash/dash.jsx
+++ frontend/components/dash/dash.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import PostCreationBar from './posts/post_creation/post_creation_bar_container';
 import PostIndex from './posts/post_index/post_index_container';
+import TextPostForm from './posts/post_creation/post_forms/text_post_form';
+import PhotoPostForm from './posts/post_creation/post_forms/photo_post_form';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
 
 class Dash extends React.Component {
     render() {
@@ -8,6 +17,10 @@ class Dash extends React.Component {
             <div>
                 <PostCreationBar />
                 <PostIndex />
+                <Switch>
+                    <Route path="/dashboard/new/text" component={TextPostForm} />
+                    <Route path="/dashboard/new/photo" component={PhotoPostForm} />
+                </Switch>
             </div>
         );
     }
