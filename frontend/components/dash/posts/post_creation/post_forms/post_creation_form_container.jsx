652bea0ee33049a61246c03be610400d5ec26ed8
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 13:33:12 -0400

    Map functions common to all post creation forms to props, with individual forms binding them as methods in constructor

--- frontend/components/dash/posts/post_creation/post_forms/post_creation_form_container.jsx
+++ frontend/components/dash/posts/post_creation/post_forms/post_creation_form_container.jsx
@@ -1,11 +1,37 @@
 import { connect } from "react-redux";
-import PostForm from './text_post_form';
-import { createPost } from "../../../../../actions/entities/post_actions";
+import { createPost } from '../../../../../actions/entities/post_actions';
+import { withRouter } from 'react-router-dom';
+import TextForm from './text_post_form';
+import { merge } from 'lodash';
 
 const msp = (state, ownProps) => ({
     post: {
-        content: "",
-        postType: ownProps.postType
+        author_id: state.session.id,
+        poster_id: state.session.id,
+        post_type: ownProps.postType
+    },
+    content: ownProps.content,
+    handleCancel: function(event) {
+        event.preventDefault();
+        this.props.history.push("/dashboard");
+    },
+    handleSubmit: function(event) {
+        event.preventDefault();
+        const content = JSON.stringify(this.state.content);
+        const post = merge({}, this.state.post);
+        post.content = content;
+        this.props.createPost(post).then( () =>
+            this.props.history.push("/dashboard")
+        );
+    },
+    handleInput: function(type) {
+        return (event) => {
+            const content = merge({}, this.state.content);
+            content[type] = event.target.value;
+            this.setState({
+                content: content
+            });
+        };
     }
 });
 
@@ -13,4 +39,4 @@ const mdp = (dispatch) => ({
     createPost: (post) => dispatch( createPost(post) )
 });
 
-export default connect(msp, mdp)(PostForm);
+export const TextPostForm = withRouter(connect(msp, mdp)(TextForm));
