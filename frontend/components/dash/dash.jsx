2812645933485aff93e876fad58f5918e288f7de
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 13:30:18 -0400

    Render post creation form container on /dashboard/new route, passing in props to define content

--- frontend/components/dash/dash.jsx
+++ frontend/components/dash/dash.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import PostCreationBar from './posts/post_creation/post_creation_bar_container';
 import PostIndex from './posts/post_index/post_index_container';
-import TextPostForm from './posts/post_creation/post_forms/text_post_form';
-import PhotoPostForm from './posts/post_creation/post_forms/photo_post_form';
 import {
-    Route,
-    Redirect,
-    Switch,
-    Link,
-    HashRouter
-} from 'react-router-dom';
+    TextPostForm
+} from './posts/post_creation/post_forms/post_creation_form_container';
+import { Route, Switch } from 'react-router-dom';
 
 class Dash extends React.Component {
     render() {
@@ -18,8 +13,14 @@ class Dash extends React.Component {
                 <PostCreationBar />
                 <PostIndex />
                 <Switch>
-                    <Route path="/dashboard/new/text" component={TextPostForm} />
-                    <Route path="/dashboard/new/photo" component={PhotoPostForm} />
+                    <Route path="/dashboard/new/text" 
+                        render={() => 
+                            <TextPostForm
+                            postType="text" 
+                            content={ {title: "", body: ""} }
+                            />
+                        } 
+                    />
                 </Switch>
             </div>
         );
