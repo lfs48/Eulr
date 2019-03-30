6ef66f2ec6ef89e1e0b0d89e4188a6f515ddf1d5
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 14:51:33 -0400

    Copy photo form template as placeholder

---
+++ frontend/components/dash/posts/post_creation/post_forms/video_post_form.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { merge } from 'lodash';
+
+class VideoPostForm extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.handleSubmit = props.handleSubmit.bind(this);
+        this.handleInput = props.handleInput.bind(this);
+        this.handleCancel = props.handleCancel.bind(this);
+        this.state = {
+            post: props.post,
+            content: props.content
+        };
+    }
+
+    render() {
+        let disabled = false;
+        if (this.state.content.url === "") {
+            disabled = true;
+        }
+        return (
+            <div className="post-form-container">
+                <div className="modal-background">
+                    <div className="modal-child" onClick={e => e.stopPropagation()}>
+                        <form onSubmit={this.handleSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Paste a URL"
+                                value={this.state.content.url}
+                                onChange={this.handleInput("url")}
+                            ></input>
+                            {disabled ?
+                            <></>
+                            :
+                            <input
+                                type="text"
+                                placeholder="Add a caption, if you like"
+                                value={this.state.content.caption}
+                                onChange={this.handleInput("caption")}
+                            ></input>
+                            }
+                            <input
+                                type="submit"
+                                value="Post"
+                                disabled={disabled}
+                            ></input>
+                            <button onClick={this.handleCancel}>Close</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    } 
+}
+
+export default VideoPostForm;
