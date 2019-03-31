8915d754f0e965e11639eb5403508d3499df1f70
lucas schraier <lucasfschraier@gmail.com>
2019-03-31 11:16:02 -0400

    Create form based on url form template

---
+++ frontend/components/dash/posts/post_creation/post_forms/audio_post_form.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { merge } from 'lodash';
+
+class AudioPostForm extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.handleSubmit = props.handleSubmit.bind(this);
+        this.handleInput = props.handleInput.bind(this);
+        this.handleCancel = props.handleCancel.bind(this);
+        this.state = {
+            post: props.post,
+            content: props.content
+        };
+    }
+
+    render() {
+        let disabled = false;
+        if (this.state.content.url === "") {
+            disabled = true;
+        }
+        return (
+            <div className="post-form-container">
+                <div className="modal-background">
+                    <div className="modal-child" onClick={e => e.stopPropagation()}>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="post-form-header">
+                                <h3>{this.props.author.username}</h3>
+                            </div>
+                            <input
+                                className="post-url-input"
+                                type="text"
+                                placeholder="Paste a URL"
+                                value={this.state.content.url}
+                                onChange={this.handleInput("url")}
+                            ></input>
+                            {disabled ?
+                            <></>
+                            :
+                            <input
+                                className="post-body-input"
+                                type="text"
+                                placeholder="Add a caption, if you like"
+                                value={this.state.content.caption}
+                                onChange={this.handleInput("caption")}
+                            ></input>
+                            }
+                            <div className="post-form-footer">
+                                <button onClick={this.handleCancel}>Close</button>
+                                <input
+                                    type="submit"
+                                    value="Post"
+                                    disabled={disabled}
+                                ></input>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    } 
+}
+
+export default AudioPostForm;
