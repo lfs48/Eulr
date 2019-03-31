fb616e72cde4caf0989bc63aeb95b35551acdf9b
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 20:34:54 -0400

    Include dash slice of state

--- frontend/reducers/ui/ui_reducer.js
+++ frontend/reducers/ui/ui_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
+import dashReducer from './dash_reducer';
 
 const uiReducer = combineReducers({
-    modal: modalReducer
+    modal: modalReducer,
+    dash: dashReducer
 });
 
 export default uiReducer;
