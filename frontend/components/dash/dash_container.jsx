59ade612d212b8a7c9dac817135db044cc05226d
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 21:01:52 -0400

    Map ui dash slice of state to connected component

--- frontend/components/dash/dash_container.jsx
+++ frontend/components/dash/dash_container.jsx
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import Dash from './dash';
 
 const msp = (state) => ({
-
+    postFormOpen: state.ui.dash === true ? true : false
 });
 
 const mdp = (dispatch) => ({
