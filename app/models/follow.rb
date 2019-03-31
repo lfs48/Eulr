f195602e52b7a4ad68f09ef40105d892844d3cbc
lucas schraier <lucasfschraier@gmail.com>
2019-03-31 11:41:09 -0400

    Create validations and associations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,15 @@
 class Follow < ApplicationRecord
+
+    validates [:follower_id, :followee_id], uniqueness: true
+
+    belongs_to :follower,
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :follower_id
+
+    belongs_to :followee,
+        class_name: :User,
+        primary_key: :id,
+        foreign_key: :followee_id
+
 end
