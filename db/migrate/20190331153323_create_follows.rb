8668bfe4656f60b17a081ba5665b91bec3c4b54f
lucas schraier <lucasfschraier@gmail.com>
2019-03-31 11:38:06 -0400

    Migrate follows

---
+++ db/migrate/20190331153323_create_follows.rb
@@ -0,0 +1,10 @@
+class CreateFollows < ActiveRecord::Migration[5.2]
+  def change
+    create_table :follows do |t|
+      t.integer :follower_id, null: false
+      t.integer :followee_id, null: false
+      t.index [:follower_id, :followee_id], unique: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_142514) do
+ActiveRecord::Schema.define(version: 2019_03_31_153323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "follows", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followee_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followee_id"], name: "index_follows_on_follower_id_and_followee_id", unique: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "author_id", null: false
