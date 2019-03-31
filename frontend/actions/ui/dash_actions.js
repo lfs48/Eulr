a69bdc4df31bfe25f4d6d2e6a8b7cee3d1dbe668
lucas schraier <lucasfschraier@gmail.com>
2019-03-30 20:44:56 -0400

    Initial commit

---
+++ frontend/actions/ui/dash_actions.js
@@ -0,0 +1,10 @@
+export const OPEN_POST_FORM = "OPEN_POST_FORM";
+export const CLEAR_DASH = "CLEAR_DASH";
+
+export const openPostForm= () => ({
+    type: OPEN_POST_FORM
+});
+
+export const clearDash = () => ({
+    type: CLEAR_DASH
+});
