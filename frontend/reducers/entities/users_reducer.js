ba724224a57fc46a102b7612d316cda0a6bab49a
lucas schraier <lucasfschraier@gmail.com>
2019-03-31 13:20:58 -0400

    Handle RECEIVE_USER_FOLLOWS action

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,6 +1,7 @@
 import { merge } from 'lodash';
 import { RECEIVE_USER, RECEIVE_ALL_USERS } from '../../actions/entities/user_actions';
 import { LOGIN_USER } from '../../actions/session/session_actions';
+import { RECEIVE_USER_FOLLOwS } from '../../actions/entities/follow_actions';
 
 const usersReducer = (state = {}, action) => {
     const newState = merge({}, state);
@@ -17,6 +18,22 @@ const usersReducer = (state = {}, action) => {
 
         case(RECEIVE_ALL_USERS): {
             return action.users;
+        }
+
+        case(RECEIVE_USER_FOLLOwS): {
+            const user = newState[action.userId]
+            if (user) {
+                const follows = Object.values(action.follows);
+                const followerIds = follows
+                    .filter( (follow) => follow.followee_id === user.id )
+                    .map( (follow) => follow.follower_id )
+                const followingIds = follows
+                .filter( (follow) => follow.follower_id === user.id )
+                .map( (follow) => follow.followee_id )
+                user.followerIds = followerIds;
+                user.followingIds = followingIds;
+            }
+            return newState;
         }
     }
 };
