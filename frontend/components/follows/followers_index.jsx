0412d1f646abc8cc90d5b5bc40d362712a05a6ca
lucas schraier <lucasfschraier@gmail.com>
2019-03-31 15:05:27 -0400

    Render follow button for users in list not already followed, which dispatches createFollow action when clicked

--- frontend/components/follows/followers_index.jsx
+++ frontend/components/follows/followers_index.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 
 class FollowersIndex extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleFollow = this.handleFollow.bind(this);
+    }
+
     componentDidMount() {
         this.props.fetchUsers().then( () =>
             this.props.fetchFollows(this.props.currentUser.id)
         );
     }
+
+    handleFollow(followeeId) {
+        return () => {
+            const follow = {
+                follower_id: this.props.currentUser.id,
+                followee_id: followeeId
+            }
+            this.props.createFollow(follow);
+        }
+    }
     
     render() {
         const lis = this.props.followers.map( (follower) => {
-            return <li key={follower.id}>{follower.username}</li>
+            return (<li key={follower.id}>
+                        <span>{follower.username}</span>
+                        {this.props.followingIds.includes(follower.id) ?
+                        <></>
+                        :
+                        <button onClick={this.handleFollow(follower.id)}>Follow</button>
+                        }
+                    </li>)
         });
 
         return (
