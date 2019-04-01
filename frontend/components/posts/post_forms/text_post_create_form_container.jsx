6eaef3bfcbf14360a9025b804786e62f4c647832
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 15:29:01 -0400

    Initial commit

---
+++ frontend/components/posts/post_forms/text_post_create_form_container.jsx
@@ -0,0 +1,50 @@
+import { connect } from "react-redux";
+import { createPost } from '../../../actions/entities/post_actions';
+import { withRouter } from 'react-router-dom';
+import { merge } from 'lodash';
+import TextPostForm from './text_post_form';
+import { clearDash } from "../../../actions/ui/dash_actions";
+
+const msp = (state, ownProps) => ({
+    post: {
+        author_id: state.session.id,
+        poster_id: state.session.id,
+        post_type: ownProps.postType
+    },
+    content: {title: "", body: ""},
+    titlePlaceholder: ownProps.titlePlaceholder,
+    bodyPlaceholder: ownProps.bodyPlaceholder,
+    author: state.entities.users[state.session.id],
+    handleCancel: function(event) {
+        event.preventDefault();
+        this.props.clearDash();
+        this.props.history.push("/dashboard");
+    },
+    handleSubmit: function(event) {
+        event.preventDefault();
+        const content = JSON.stringify(this.state.content);
+        const post = merge({}, this.state.post);
+        post.content = content;
+        this.props.createPost(post).then( () => {
+            this.props.clearDash();
+            this.props.history.push("/dashboard");
+            }
+        );
+    },
+    handleInput: function(type) {
+        return (event) => {
+            const content = merge({}, this.state.content);
+            content[type] = event.target.value;
+            this.setState({
+                content: content
+            });
+        };
+    }
+});
+
+const mdp = (dispatch) => ({
+    createPost: (post) => dispatch( createPost(post) ),
+    clearDash: () => dispatch( clearDash() )
+});
+
+export default withRouter(connect(msp, mdp)(TextPostForm));
---
+++ frontend/components/posts/post_forms/text_post_form.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+class TextPostForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = props.handleSubmit.bind(this);
+        this.handleInput = props.handleInput.bind(this);
+        this.handleCancel = props.handleCancel.bind(this);
+        this.state = {
+            post: props.post,
+            content: props.content
+        };
+    }
+
+    render() {
+        let disabled = false;
+        if (this.state.content.title === "" && this.state.content.body === "") {
+            disabled = true;
+        }
+        return (
+            <div className="post-form-container">
+                <div className="modal-background">
+                    <div className="modal-child" onClick={e => e.stopPropagation()}>
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="post-form-header">
+                                <h3>{this.props.author.username}</h3>
+                            </div>
+                            <input
+                                className="post-title-input"
+                                type="text"
+                                placeholder={this.props.titlePlaceholder}
+                                value={this.state.content.title}
+                                onChange={this.handleInput("title")}
+                            ></input>
+                            <input
+                                className="post-body-input"
+                                type="text"
+                                placeholder={this.props.bodyPlaceholder}
+                                value={this.state.content.body}
+                                onChange={this.handleInput("body")}
+                            ></input>
+                            <div className="post-form-footer">
+                                <button onClick={this.handleCancel}>Close</button>
+                                <input
+                                    type="submit"
+                                    value="Post"
+                                    disabled={disabled}
+                                ></input>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TextPostForm;
