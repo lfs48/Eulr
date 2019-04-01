d07061201cb95750bdb30c2e8de8c8942d2103f9
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 17:25:46 -0400

    Map currentUser to props

--- frontend/components/dash/dash_header_container.jsx
+++ frontend/components/dash/dash_header_container.jsx
@@ -1,13 +1,13 @@
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import DashHeader from './dash_header';
 
 const msp = (state) => ({
-
+    currentUser: state.entities.users[state.session.id]
 });
 
 const mdp = (dispatch) => ({
 
 });
 
-export default withRouter(connect(mdsp, mdp)(DashHeader));
+export default withRouter( connect(msp, mdp)(DashHeader) );
