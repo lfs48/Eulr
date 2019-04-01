7caa669b58e6a79200d30021e0617c699571a00b
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 14:33:29 -0400

    Render text posts

--- frontend/components/dash/posts/post_index/post_index_item/post_index_item.jsx
+++ frontend/components/dash/posts/post_index/post_index_item/post_index_item.jsx
@@ -20,19 +20,42 @@ class PostIndexItem extends React.Component {
         this.props.author ? null : this.props.fetchUser(this.props.post.author_id);
     }
 
+    formatContent(post) {
+        const content = JSON.parse(post.content);
+        switch(post.post_type) {
+            case("text"): {
+                return (
+                    <div className="text-post-body">
+                    <h4>{content.title}</h4>
+                    <p>{content.body}</p>
+                    </div>
+                );
+            }
+        }
+    }
+
     render() {
         return(
-            <div>
-                <h4>{this.props.author ? this.props.author.username : ""}</h4>
-                <span>{this.props.post.content}</span>
-                {this.props.isOwnPost ? 
-                (<>
-                <button onClick={this.handleEdit}>Edit</button>
-                <button onClick={this.handleDelete}>Delete</button>
-                </>)
-                :
-                <button>Reblog</button>
-                }
+            <div className="post-avatar-wrapper">
+                <img className="avatar" src={this.props.author.avatar}></img>
+                <div className="post-container">
+                    <div className="post-header-container">
+                        <h4>{this.props.author ? this.props.author.username : ""}</h4>
+                    </div>
+                    <div className="post-body-container">
+                    {this.formatContent(this.props.post)}
+                    </div>
+                    <div className="post-footer-container">
+                    {this.props.isOwnPost ? 
+                    (<>
+                    <button onClick={this.handleEdit}>Edit</button>
+                    <button onClick={this.handleDelete}>Delete</button>
+                    </>)
+                    :
+                    <button>Reblog</button>
+                    }
+                    </div>
+                </div>
             </div>
         )
     }
