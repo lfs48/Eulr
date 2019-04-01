505a3698ee740f023b0614d6a6bcad533fd3d309
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 17:23:38 -0400

    Remove dash slice of state, no longer neccesary

--- frontend/reducers/ui/ui_reducer.js
+++ frontend/reducers/ui/ui_reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
-import dashReducer from './dash_reducer';
 import navReducer from './nav_reducer';
 
 const uiReducer = combineReducers({
     modal: modalReducer,
-    dash: dashReducer,
     nav: navReducer
 });
 
