5a5b010c42a6dfd4a272c615ccf1fc086666a4e9
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 17:25:09 -0400

    Copy route code formerly in dash component

--- frontend/components/dash/dash_header.jsx
+++ frontend/components/dash/dash_header.jsx
@@ -1,8 +1,112 @@
-import react from 'react';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import PostCreationBar from './posts/post_creation/post_creation_bar_container';
+import {
+    PhotoPostForm,
+    LinkPostForm,
+    VideoPostForm
+} from './posts/post_creation/post_forms/post_creation_form_container';
+import TextPostCreateForm from '../posts/post_forms/text_post_create_form_container';
+import TextPostEditForm from '../posts/post_forms/text_post_edit_container';
 
 class DashHeader extends React.Component {
-    reder() {
-        
+    render() {
+        return (
+            <div className="dash-header-container">
+                <img className="avatar" src={this.props.currentUser.avatar}></img>
+                <Switch>
+                    <Route exact path="/dashboard" component={PostCreationBar} />
+                        }
+                    />
+                    <Route path="/dashboard/new/text"
+                        render={() =>
+                            <>
+                            <TextPostCreateForm
+                                postType="text"
+                                titlePlaceholder="Title"
+                                bodyPlaceholder="Your text here"
+                            />
+                            <div className="modal-background"></div>
+                            </>
+                        }
+                    />
+                    <Route path="/dashboard/new/photo"
+                        render={() =>
+                            <PhotoPostForm
+                                postType="photo"
+                                content={{ url: "", caption: "" }}
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/new/quote"
+                        render={() =>
+                            <TextPostCreateForm
+                                postType="quote"
+                                titlePlaceholder="Quote"
+                                bodyPlaceholder="-Source"
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/new/link"
+                        render={() =>
+                            <LinkPostForm
+                                postType="link"
+                                content={{ url: "", title: "", summary: "", description: "" }}
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/new/chat"
+                        render={() =>
+                            <TextPostCreateForm
+                                postType="chat"
+                                titlePlaceholder="Title"
+                                bodyPlaceholder="Bob: Hello!"
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/new/audio"
+                        render={() =>
+                            <VideoPostForm
+                                postType="audio"
+                                content={{ url: "", caption: "" }}
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/new/video"
+                        render={() =>
+                            <VideoPostForm
+                                postType="video"
+                                content={{ url: "", caption: "" }}
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/edit/text/:postId"
+                        render={() =>
+                            <TextPostEditForm
+                                titlePlaceholder="Title"
+                                bodyPlaceholder="Your text here"
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/edit/quote/:postId"
+                        render={() =>
+                            <TextPostEditForm
+                                titlePlaceholder="Quote"
+                                bodyPlaceholder="-Source"
+                            />
+                        }
+                    />
+                    <Route path="/dashboard/edit/chat/:postId"
+                        render={() =>
+                            <TextPostEditForm
+                                titlePlaceholder="Title"
+                                bodyPlaceholder="Bob: Hello"
+                            />
+                        }
+                    />
+                </Switch>
+            </div>
+        );
     }
 }
 
