c28d15264c90cd14ca3f0fe7d8d9713d0ee1b89f
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 11:59:08 -0400

    Map current user avatar to props

--- frontend/components/dash/dash_container.jsx
+++ frontend/components/dash/dash_container.jsx
@@ -2,7 +2,8 @@ import { connect } from "react-redux";
 import Dash from './dash';
 
 const msp = (state) => ({
-    postFormOpen: state.ui.dash === true ? true : false
+    postFormOpen: state.ui.dash === true ? true : false,
+    currentUser: state.entities.users[state.session.id]
 });
 
 const mdp = (dispatch) => ({
