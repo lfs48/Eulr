622199851a290b8fac78c29df6f1e502c1a97147
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-01 11:38:20 -0400

    Migrate deletion of avatar_url column in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_01_133649) do
+ActiveRecord::Schema.define(version: 2019_04_01_153333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 2019_04_01_133649) do
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "email", null: false
-    t.string "avatar_url", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.datetime "created_at", null: false
