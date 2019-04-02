dacb51b9781f1f12ac04b9b0543457295551700a
lucas schraier <lucasfschraier@gmail.com>
2019-04-01 22:21:52 -0400

    Fetch all users when rendering post index to prevent possible crash

--- frontend/components/dash/posts/post_index/post_index.jsx
+++ frontend/components/dash/posts/post_index/post_index.jsx
@@ -8,6 +8,7 @@ class PostIndex extends React.Component {
     }
 
     componentDidMount() {
+        this.props.fetchUsers();
         this.props.fetchPosts();
     }
 
--- frontend/components/dash/posts/post_index/post_index_container.jsx
+++ frontend/components/dash/posts/post_index/post_index_container.jsx
@@ -1,13 +1,15 @@
 import { connect } from 'react-redux';
 import PostIndex from './post_index';
 import { fetchPosts } from '../../../../actions/entities/post_actions';
+import { fetchUsers } from '../../../../actions/entities/user_actions';
 
 const msp = (state) => ({
     posts: Object.values(state.entities.posts)
 });
 
 const mdp = (dispatch) => ({
-    fetchPosts: () => dispatch( fetchPosts() )
+    fetchPosts: () => dispatch( fetchPosts() ),
+    fetchUsers: () => dispatch( fetchUsers() )
 });
 
 export default connect(msp, mdp)(PostIndex);
