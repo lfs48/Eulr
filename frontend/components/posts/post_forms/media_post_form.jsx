602204972b47962f8e9ac26a0b15ee09dc124aa7
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-02 09:57:45 -0400

    Replace hardcoded post info with actual post info retrieved from props/state

--- frontend/components/posts/post_forms/media_post_form.jsx
+++ frontend/components/posts/post_forms/media_post_form.jsx
@@ -1,53 +1,86 @@
 import React from 'react';
+import { merge } from 'lodash';
 
 class MediaForm extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleInput = this.handleInput.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleUpload = this.handleUpload.bind(this);
-        this.state = {};
+        this.state = {
+            post: props.post,
+            content: props.content
+        };
+    }
+
+    handleInput(type) {
+        return (event) => {
+            const content = merge({}, this.state.content);
+            content[type] = event.target.value;
+            this.setState({
+                content: content
+            });
+        };
     }
 
     handleUpload(event) {
         const reader = new FileReader();
         const file = event.currentTarget.files[0];
         reader.onloadend = () =>
-            this.setState({ imageUrl: reader.result, imageFile: file });
-
+            this.setState({
+                media: file
+            });
         if (file) {
             reader.readAsDataURL(file);
         } else {
-            this.setState({ imageUrl: "", imageFile: null });
+            this.setState({
+                media: null
+            });
         }
     }
 
+    // handleSubmit: function(event) {
+    // event.preventDefault();
+    // const content = JSON.stringify(this.state.content);
+    // const post = merge({}, this.state.post);
+    // post.content = content;
+    // this.props.createPost(post).then(() => {
+    //     this.props.clearDash();
+    //     this.props.history.push("/dashboard");
+    // }
+    // );
+    // }
+
     handleSubmit(event) {
         event.preventDefault();
+        const content = JSON.stringify(this.state.content);
         const formData = new FormData();
-        formData.append('post[content]', "{body: stuff}");
-        formData.append('post[author_id]', 1);
-        formData.append('post[poster_id]', 1);
-        formData.append('post[post_type]', "photo");
-        if (this.state.imageFile) {
-
-            formData.append('post[media]', this.state.imageFile);
+        formData.append("post[author_id]", this.state.post.author_id);
+        formData.append("post[poster_id]", this.state.post.poster_id);
+        formData.append("post[post_type]", this.state.post.post_type);
+        formData.append("post[content]", content);
+        if (this.state.media) {
+            formData.append("post[media]", this.state.media);
         }
-        $.ajax({
-            url: '/api/posts',
-            method: 'POST',
-            data: formData,
-            contentType: false,
-            processData: false
-        });
+        this.props.createPost(formData).then( () =>
+            this.props.history.push("/dashboard")
+        );
     }
 
     render() {
         return(
-            <div>
+            <div className="modal-child">
             <form onSubmit={this.handleSubmit}>
                 <input type="file" onChange={this.handleUpload}></input>
-                <input type="submit" value="upload"></input>
+                    <input
+                        className="post-caption-input"
+                        type="text"
+                        placeholder="Enter a caption, if you like"
+                        value={this.state.content.caption}
+                        onChange={this.handleInput("caption")}
+                    ></input>
+                <input type="submit" value="Post"></input>
             </form>
             </div>
         );
