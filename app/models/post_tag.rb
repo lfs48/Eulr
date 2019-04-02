0069079e29675ae8ab7c816360781db82c7192de
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-02 18:33:57 -0400

    Add inverse_of option to relations

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -2,8 +2,8 @@ class PostTag < ApplicationRecord
 
     validates :post_id, uniqueness: {scope: :tag_id}
 
-    belongs_to :post
+    belongs_to :post, inverse_of: :tags
 
-    belongs_to :tag
+    belongs_to :tag, inverse_of: :tagged_post
 
 end
