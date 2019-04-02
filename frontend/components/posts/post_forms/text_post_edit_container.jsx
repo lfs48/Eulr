f5476091ac2a2b9876729471e2a2a1bef52aa983
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-02 15:24:12 -0400

    Change name of mapped action to formAction

--- frontend/components/posts/post_forms/text_post_edit_container.jsx
+++ frontend/components/posts/post_forms/text_post_edit_container.jsx
@@ -6,42 +6,19 @@ import { merge } from 'lodash';
 import TextPostForm from './text_post_form';
 
 const msp = (state, ownProps) => {
-    const post = state.entities.posts[ownProps.match.params.postId]
+    const post = state.entities.posts[ownProps.match.params.postId];
     return({
         post: post,
         content: JSON.parse(post.content),
         titlePlaceholder: ownProps.titlePlaceholder,
         bodyPlaceholder: ownProps.bodyPlaceholder,
         author: state.entities.users[post.author_id],
-        handleCancel: function(event) {
-            event.preventDefault();
-            this.props.history.push("/dashboard");
-        },
-        handleSubmit: function(event) {
-            event.preventDefault();
-            const content = JSON.stringify(this.state.content);
-            const post = merge({}, this.state.post);
-            post.content = content;
-            this.props.updatePost(post).then( () =>
-                this.props.history.push("/dashboard")
-            );
-        },
-        handleInput: function(type) {
-            return (event) => {
-                const log = this.state;
-                const content = merge({}, this.state.content);
-                content[type] = event.target.value;
-                this.setState({
-                    content: content
-                });
-            };
-        }
     });
-}
+};
 
 const mdp = (dispatch) => ({
     fetchPost: (id) => dispatch( fetchPost(id) ),
-    updatePost: (post) => dispatch( updatePost(post) )
+    formAction: (post) => dispatch( updatePost(post) )
 });
 
 export default withRouter(connect(msp, mdp)(TextPostForm));
