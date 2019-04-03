b9bc17613139aff09db17d6a4772120bf357dca6
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-03 11:03:21 -0400

    Include tags slice of state

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
 import followsReducer from './follows_reducer';
+import tagsReducer from './tags_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     posts: postsReducer,
-    follows: followsReducer
+    follows: followsReducer,
+    tags: tagsReducer
 });
 
 export default entitiesReducer;
