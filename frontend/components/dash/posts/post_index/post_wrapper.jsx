a1ca7174642fdad16ce7a7523cf412fe6774713d
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-03 19:31:00 -0400

    Add condition to render link edit form

--- frontend/components/dash/posts/post_index/post_wrapper.jsx
+++ frontend/components/dash/posts/post_index/post_wrapper.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PostIndexItem from './post_index_item/post_index_item_container';
-import TextPostEditForm from '../../../posts/post_forms/text_post_edit_container';
+import {TextPostEditForm, LinkPostEditForm } from '../../../posts/post_forms/text_post_edit_container';
 import MediaPostEditForm from '../../../posts/post_forms/media_post_edit_container';
 
 const PostWrapper = (props) => {
@@ -11,8 +11,11 @@ const PostWrapper = (props) => {
                 (props.post.post_type === 'text' || props.post.post_type === 'quote' || props.post.post_type === 'chat') ?
                     <TextPostEditForm post={props.post} />
                     :
-                    <MediaPostEditForm post={props.post} />
-                :
+                        (props.post.post_type === "photo" || props.post.post_type === "audio" || props.post.post_type === "video") ?
+                            <MediaPostEditForm post={props.post} />
+                            :
+                                <LinkPostEditForm post={props.post} />
+        :
                 <PostIndexItem post={props.post} />
             }
         </div>
