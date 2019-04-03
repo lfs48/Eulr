2fca18d1a935a6c154b728d2ec87d04a88cff9d8
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-03 09:36:13 -0400

    Define method to create tags from array of strings

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -8,4 +8,14 @@ class Tag < ApplicationRecord
         through: :post_tags,
         source: :post
 
+    def self.create_tags_from_strings(tagStrs)
+        tags = Tag.where(:tag => tagStrs)
+        tagStrs.each do |tagStr|
+            unless ( tags.any? { |record| record.tag == tagStr } )
+                tag = Tag.new({tag: tagStr})
+                tag.save
+            end
+        end
+    end
+
 end
