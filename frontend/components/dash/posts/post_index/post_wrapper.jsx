9ce61f158cbb53c2e8682fa271831d651fd7ef9b
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-03 16:17:08 -0400

    Conditionally render appropriate post edit form for post type

--- frontend/components/dash/posts/post_index/post_wrapper.jsx
+++ frontend/components/dash/posts/post_index/post_wrapper.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import PostIndexItem from './post_index_item/post_index_item_container';
 import TextPostEditForm from '../../../posts/post_forms/text_post_edit_container';
+import MediaPostEditForm from '../../../posts/post_forms/media_post_edit_container';
 
 const PostWrapper = (props) => {
     return(
         <div className="post-avatar-wrapper">
             <img className="avatar" src={props.author.avatar}></img>
             {props.edit ?
-                <TextPostEditForm post={props.post} />
+                (props.post.post_type === 'text' || props.post.post_type === 'quote' || props.post.post_type === 'chat') ?
+                    <TextPostEditForm post={props.post} />
+                    :
+                    <MediaPostEditForm post={props.post} />
                 :
                 <PostIndexItem post={props.post} />
             }
