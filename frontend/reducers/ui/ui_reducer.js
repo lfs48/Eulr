6c859df907cf0bf56b7fef3b5519688086984f9a
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-03 13:30:23 -0400

    Include postIndex slice of state

--- frontend/reducers/ui/ui_reducer.js
+++ frontend/reducers/ui/ui_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
 import navReducer from './nav_reducer';
+import postIndexReducer from './post_index_reducer';
 
 const uiReducer = combineReducers({
     modal: modalReducer,
-    nav: navReducer
+    nav: navReducer,
+    postIndex: postIndexReducer
 });
 
 export default uiReducer;
