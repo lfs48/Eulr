782626c400e52985dcb44b7a3b4e43456beebb8a
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-04 15:35:43 -0400

    Add css classes and container elements for styling

--- frontend/components/dash/dash_sidebar.jsx
+++ frontend/components/dash/dash_sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class DashSidebar extends React.Component {
 
@@ -6,17 +7,25 @@ class DashSidebar extends React.Component {
         const lis = this.props.users.map( (user) => {
             return(
                 <li key={user.id}>
-                    <img src={user.avatar}></img>
+                    <div>
+                    <img className="avatar-small" src={user.avatar}></img>
                     <span>{user.username}</span>
-                    <button>Follow</button>
+                    </div>
+                    <button>
+                        <i className="fas fa-plus"></i>
+                    </button>
                 </li>
             );
         });
         return (
             <div className="dash-sidebar-container">
+                <h3>Recommended Blogs</h3>
                 <ul>
                     {lis}
                 </ul>
+                <Link to="/explore" >
+                    <span>Explore all of Tumblr</span>
+                </Link>
             </div>
         );
     }
