f7dd489592d391a9cba310050eacf21b8831a108
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-04 14:56:42 -0400

    Generate list of random users, excluding the current user, to display on the sidebar

--- frontend/components/dash/dash_sidebar_container.jsx
+++ frontend/components/dash/dash_sidebar_container.jsx
@@ -2,20 +2,19 @@ import { connect } from 'react-redux';
 import DashSidebar from './dash_sidebar';
 import { fetchUsers } from '../../actions/entities/user_actions';
 
-const msp = (state) => {
-    const currentUser = state.entities.users[state.session.id];
+const msp = (state, ownProps) => {
+    const currentUser = ownProps.currentUser;
     const users = Object.values(state.entities.users);
     let randomUsers = [];
     while (randomUsers.length < 2) {
         const randomUser = users[Math.floor(Math.random() * users.length)];
-        // if (randomUsers.includes(randomUser) || randomUser.id === currentUser.id ) {
-        //     continue;
-        // }
+        if ( randomUsers.includes(randomUser) || randomUser.id === currentUser.id) {
+            continue;
+        }
         randomUsers.push(randomUser);
     }
     return ({
         users: randomUsers,
-        currentUser: currentUser
     });
 };
 
