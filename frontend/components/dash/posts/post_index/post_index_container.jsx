bdf36d55be477bfc5a1c23609437679fad33b543
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-04 11:17:56 -0400

    Map current user to connected component

--- frontend/components/dash/posts/post_index/post_index_container.jsx
+++ frontend/components/dash/posts/post_index/post_index_container.jsx
@@ -3,14 +3,19 @@ import PostIndex from './post_index';
 import { fetchPosts } from '../../../../actions/entities/post_actions';
 import { fetchUsers } from '../../../../actions/entities/user_actions';
 import { fetchTags } from '../../../../actions/entities/tag_actions';
+import { fetchFollows } from '../../../../actions/entities/follow_actions';
 
-const msp = (state) => ({
-    posts: Object.values(state.entities.posts),
-    editId: state.ui.postIndex
-});
+const msp = (state) => {
+    return({
+        posts: Object.values(state.entities.posts),
+        currentUser: state.entities.users[state.session.id],
+        editId: state.ui.postIndex
+    });
+}
 
 const mdp = (dispatch) => ({
-    fetchPosts: () => dispatch( fetchPosts() ),
+    fetchFollows: (id) => dispatch( fetchFollows(id) ),
+    fetchPosts: (authorIds) => dispatch( fetchPosts(authorIds) ),
     fetchUsers: () => dispatch( fetchUsers() ),
     fetchTags: () => dispatch( fetchTags() )
 });
