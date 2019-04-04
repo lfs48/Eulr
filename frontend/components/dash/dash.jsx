7929c6fc3d28f172be8a13f4040a23c6bdb735b8
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-04 14:54:59 -0400

    Render sidebar, add container elements for styling

--- frontend/components/dash/dash.jsx
+++ frontend/components/dash/dash.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostIndex from './posts/post_index/post_index_container';
 import DashHeader from './dash_header_container';
+import DashSidebar from './dash_sidebar_container';
 
 class Dash extends React.Component {
 
@@ -25,8 +26,13 @@ class Dash extends React.Component {
         if (this.state.loaded) {
             return (
                 <div className="dash-container">
-                    <DashHeader />
-                    <PostIndex />
+                    <section className="dash-center-column">
+                        <DashHeader />
+                        <PostIndex />
+                    </section>
+                    <section className="dash-right-column">
+                        <DashSidebar currentUser={this.props.currentUser} />
+                    </section>
                 </div>
             );
         } else {
