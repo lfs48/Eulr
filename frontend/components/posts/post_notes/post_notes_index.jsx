911b497e110450cbfa8b718e39e5e82a4e11b76e
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 10:48:38 -0400

    Define methods to open and close list, render button which opens list when clicked

--- frontend/components/posts/post_notes/post_notes_index.jsx
+++ frontend/components/posts/post_notes/post_notes_index.jsx
@@ -1,26 +1,52 @@
 import React from 'react';
 
-class PostNotesIndex {
+class PostNotesIndex extends React.Component {
 
     constructor(props) {
         super(props);
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
         this.state = {
             open: false
         }
     }
 
+    handleOpen(event) {
+        event.preventDefault();
+        this.setState({
+            open: true
+        },
+        () => document.addEventListener("click", this.handleClose)
+        );
+    }
+
+    handleClose(event) {
+        event.preventDefault();
+        this.setState({
+            open: false
+        },
+        () => document.removeEventListener("click", this.handleClose)
+        );
+    }
+
     render() {
-        const lis = props.likers.map( (liker, idx) => 
+        const lis = this.props.likers.map( (liker, idx) => 
             <li key={idx}>
                 <img className="avatar-tiny" src={liker.avatar_url}></img>
                 <span>{liker.username}</span>
             </li>
         )
-        return(
-            <ul>
-                {lis}
-            </ul>
-        );
+        if (this.state.open) {
+            return(
+                <ul>
+                    {lis}
+                </ul>
+            );
+        } else {
+            return(
+                <button onClick={this.handleOpen}>{this.props.likers.length}</button>
+            );
+        }
     }
 }
 
