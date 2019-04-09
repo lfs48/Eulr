a3c9b3382a2a16e070c99c4c34e41146cec5df63
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 15:12:22 -0400

    Include inverse_of option in associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,8 @@ class Like < ApplicationRecord
 
     validates :post_id, uniqueness: {scope: :user_id}
 
-    belongs_to :post
+    belongs_to :post, inverse_of: :likes
 
-    belongs_to :user
+    belongs_to :user, inverse_of: :likes
 
 end
