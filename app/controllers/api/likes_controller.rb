a06ff3f369a90b8626aa44128f7302d539b8afa1
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 15:17:14 -0400

    Define create and destroy actions

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,11 +1,19 @@
 class Api::LikesController < ApplicationController
     
     def create
-
+        @post = Post.find_by(id: params[:postId])
+        if @post.likes.create(likes_params)
+            render "api/posts/post"
+        else
+            render json: @post.errors.full_messages, status: 422
+        end
     end
 
     def destroy
-
+        like = Like.find_by(post_id: params[:post_id], user_id: params[:user_id])
+        id = like.id
+        like.destroy
+        render json: id
     end
 
     private
