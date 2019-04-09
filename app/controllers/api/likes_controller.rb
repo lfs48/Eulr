cfb786c463899cd716cd2ba9d53732cb96d74a4f
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 15:44:49 -0400

    Fix typos

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,19 +1,19 @@
 class Api::LikesController < ApplicationController
     
     def create
-        @post = Post.find_by(id: params[:postId])
+        @post = Post.find_by(id: params[:like][:post_id])
         if @post.likes.create(likes_params)
-            render "api/posts/post"
+            render "api/posts/show"
         else
             render json: @post.errors.full_messages, status: 422
         end
     end
 
     def destroy
-        like = Like.find_by(post_id: params[:post_id], user_id: params[:user_id])
-        id = like.id
+        like = Like.find_by(post_id: params[:like][:post_id], user_id: params[:like][:user_id])
+        @post = like.post
         like.destroy
-        render json: id
+        render "api/posts/show"
     end
 
     private
