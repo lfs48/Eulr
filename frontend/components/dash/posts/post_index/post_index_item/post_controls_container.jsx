a0179858078a7d1f421b2d60204987000725b5ef
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 16:44:43 -0400

    Map state and dispatch to props

--- frontend/components/dash/posts/post_index/post_index_item/post_controls_container.jsx
+++ frontend/components/dash/posts/post_index/post_index_item/post_controls_container.jsx
@@ -1,12 +1,24 @@
 import { connect } from 'react-redux';
 import PostControls from './post_controls';
+import { likePost, unlikePost, deletePost } from '../../../../../actions/entities/post_actions';
+import { openEditForm } from '../../../../../actions/ui/post_index_actions';
 
-const msp = (state, ownProps) => ({
-    post: ownProps.post
-});
+const msp = (state, ownProps) => {
+    const post = ownProps.post;
+    const currentUser = state.entities.users[state.session.id]
+    return({
+        post: post,
+        currentUser: currentUser,
+        isOwnPost: state.session.id === post.author_id,
+        isLiked: currentUser.likes.includes(post.id)
+    });
+};
 
 const mdp = (dispatch) => ({
-
+    likePost: (like) => dispatch( likePost(like) ),
+    unlikePost: (like) => dispatch( unlikePost(like) ),
+    deletePost: (id) => dispatch( deletePost(id) ),
+    openEditForm: (id) => dispatch( openEditForm(id) )
 })
 
 export default connect(msp, mdp)(PostControls);
