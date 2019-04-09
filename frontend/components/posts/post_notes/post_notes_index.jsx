206238d248aabcb8b982d1f2b8211221f67d0406
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-09 10:42:31 -0400

    Convert to class component due to need for internal state

--- frontend/components/posts/post_notes/post_notes_index.jsx
+++ frontend/components/posts/post_notes/post_notes_index.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
-const PostNotesIndex = (props) => {
-    const lis = props.likers.map( (liker, idx) => 
-        <li key={idx}>
-            <img className="avatar-tiny" src={liker.avatar_url}></img>
-            <span>{liker.username}</span>
-        </li>
-    )
-    return(
-        <ul>
-            {lis}
-        </ul>
-    );
+class PostNotesIndex {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+
+    render() {
+        const lis = props.likers.map( (liker, idx) => 
+            <li key={idx}>
+                <img className="avatar-tiny" src={liker.avatar_url}></img>
+                <span>{liker.username}</span>
+            </li>
+        )
+        return(
+            <ul>
+                {lis}
+            </ul>
+        );
+    }
 }
 
 export default PostNotesIndex;
