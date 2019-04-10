79ac75d3080cd58cadab8f337cfeeabc207e4ce7
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-10 15:16:16 -0400

    Remove follows slice of state

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
-import followsReducer from './follows_reducer';
 import tagsReducer from './tags_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     posts: postsReducer,
-    follows: followsReducer,
     tags: tagsReducer
 });
 
