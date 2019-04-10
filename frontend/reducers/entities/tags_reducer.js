ebfb52e9e1b247694da40b59fe68d03352f5e2ad
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-10 11:41:49 -0400

    Handle RECEIVE_ALL_POSTS and RECEIVE_POST action, cease handling RECEIVE_ALL_TAGS action

--- frontend/reducers/entities/tags_reducer.js
+++ frontend/reducers/entities/tags_reducer.js
@@ -1,5 +1,6 @@
 import { merge } from 'lodash';
 import { RECEIVE_ALL_TAGS } from '../../actions/entities/tag_actions';
+import { RECEIVE_ALL_POSTS, RECEIVE_POST } from '../../actions/entities/post_actions';
 
 const tagsReducer = (state = {}, action) => {
     const newState = merge({}, state);
@@ -8,8 +9,13 @@ const tagsReducer = (state = {}, action) => {
     switch(action.type) {
         default: return state;
 
-        case(RECEIVE_ALL_TAGS): {
-            return action.tags
+        case(RECEIVE_ALL_POSTS): {
+            return action.payload.tags
+        }
+
+        case(RECEIVE_POST): {
+            const tags = payload.tags;
+            return merge(newState, tags);
         }
     }
 };
