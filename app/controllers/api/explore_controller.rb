01fd2bdb99bc235c1b26f11e4a0563c1030f7885
Lucas Schraier <lucasfschraier@gmail.com>
2019-04-18 11:50:07 -0400

    Fix SQL query syntax

--- app/controllers/api/explore_controller.rb
+++ app/controllers/api/explore_controller.rb
@@ -4,25 +4,22 @@ class Api::ExploreController < ApplicationController
     def index
         sql  = "
             SELECT
-                *
+                posts.*, COUNT(likes.id)
             FROM
                 posts
-            JOIN
-                users
-            ON
-                users.id = posts.author_id
             JOIN
                 likes
             ON 
                 likes.post_id = posts.id
             GROUP BY
-                posts.id, users.id, likes.id
+                posts.id
             ORDER BY
-                COUNT(likes.id)
+                COUNT(likes.id) desc
             LIMIT
-                5
+                10
+            ;
         "
-        @posts = Post.with_attached_media.find_by_sql(sql);
+        @posts = Post.includes(:author, :tags, :likers).with_attached_media.find_by_sql(sql);
         postIds = @posts.map { |post| post.id }
         tagIds = PostTag.where( :post_id => postIds).map { |postTag| postTag.tag_id }
         @tags = Tag.where( :id => tagIds )
