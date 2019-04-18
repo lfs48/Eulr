99b3ba4c7388760aa5556b873ab24d950c9273d3
lucas schraier <lucasfschraier@gmail.com>
2019-04-17 22:09:08 -0400

    Handle clicking follow button by following that user

--- frontend/components/dash/dash_sidebar.jsx
+++ frontend/components/dash/dash_sidebar.jsx
@@ -3,15 +3,32 @@ import { Link } from 'react-router-dom';
 
 class DashSidebar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: props.users
+        }
+    }
+
+    handleFollow(followeeId) {
+        return () => {
+            const follow = {
+                follower_id: this.props.currentUser.id,
+                followee_id: followeeId
+            }
+            this.props.createFollow(follow);
+        }
+    }
+
     render() {
-        const lis = this.props.users.map( (user) => {
+        const lis = this.state.users.map( (user) => {
             return(
                 <li key={user.id}>
                     <div>
                     <img className="avatar-small" src={user.avatar}></img>
                     <span>{user.username}</span>
                     </div>
-                    <button>
+                    <button onClick={this.handleFollow(user.id)}>
                         <i className="fas fa-plus"></i>
                     </button>
                 </li>
@@ -24,7 +41,7 @@ class DashSidebar extends React.Component {
                     {lis}
                 </ul>
                 <Link to="/explore" >
-                    <span>Explore all of Tumblr</span>
+                    <span>Explore all of Eulr</span>
                 </Link>
             </div>
         );
