17159d75e9d965f5a8e6c749a6e2e34da683bef5
lucas schraier <lucasfschraier@gmail.com>
2019-04-20 17:05:34 -0400

    Refactor alg for selecting random blogs so that app doesn't crash when the user follows too many blogs

--- frontend/components/dash/dash_sidebar_container.jsx
+++ frontend/components/dash/dash_sidebar_container.jsx
@@ -4,17 +4,18 @@ import { createFollow } from '../../actions/entities/follow_actions';
 
 const msp = (state, ownProps) => {
     const currentUser = state.entities.users[state.session.id]
-    const users = Object.values(state.entities.users);
+    const users = Object.values(state.entities.users).filter( user => !(currentUser.followingIds.includes(user.id) || currentUser.id === user.id) );
     let randomUsers = [];
-    while (randomUsers.length < 5) {
+    while (randomUsers.length < Math.min(users.length, 10) ) {
         const randomUser = users[Math.floor(Math.random() * users.length)];
-        if ( randomUsers.includes(randomUser) || randomUser.id === currentUser.id || currentUser.followingIds.includes(randomUser.id)) {
+        if ( randomUsers.includes(randomUser) ) {
             continue;
         }
         randomUsers.push(randomUser);
     }
     return ({
         users: randomUsers,
+        currentUser: currentUser
     });
 };
 
