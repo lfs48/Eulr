63de20d7fa6d22ac388b605f925829256dc588a8
lucas schraier <lucasfschraier@gmail.com>
2019-04-20 13:38:54 -0400

    Add custom avatars to seeded users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
 user_list = [
-    ["euler", "leuler@mathmail.com", "123456789"],
-    ["fermat", "pfermat@mathmail.com", "123456789"],
-    ["gauss", "cfgauss@mathmail.com", "123456789"],
-    ["leibniz", "gwleibniz@mathmail.com", "123456789"],
-    ["euclid", "euclid@mathmail.com", "123456789"],
-    ["lovelace", "alovelace@mathmail.com", "123456789"],
-    ["turing", "aturing@mathmail.com", "123456789"],
-    ["hilbert", "dhilbert@mathmail.com", "123456789"],
-    ["riemann", "briemann@mathmail.com", "123456789"],
-    ["germain", "sgermain@mathmail.com", "123456789"],
-    ["fibonacci", "lfibonacci@mathmail.com", "123456789"],
-    ["noether", "enoether@mathmail.com", "123456789"],
-    ["wiles", "awiles@mathmail.com", "123456789"],
-    ["fourier", "jfourier@mathmail.com", "123456789"],
-    ["galois", "egalois@mathmail.com", "123456789"],
-    ["cantor", "gcantor@mathmail.com", "123456789"],
-    ["ramanujan", "sramanujan@mathmail.com", "123456789"],
-    ["newton", "inewton@mathmail.com", "123456789"],
-    ["kovalevskaya", "skovalevskaya@mathmail.com", "123456789"],
-    ["cartwright", "mcartwright@mathmail.com", "123456789"],
+    ["euler", "leuler@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/d/d7/Leonhard_Euler.jpg"],
+    ["fermat", "pfermat@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/f/f3/Pierre_de_Fermat.jpg"],
+    ["gauss", "cfgauss@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/9/9b/Carl_Friedrich_Gauss.jpg"],
+    ["leibniz", "gwleibniz@mathmail.com", "123456789", "https://www.college.columbia.edu/core/sites/core/files/styles/large/public/images/Gottfried_Wilhelm_von_Leibniz.jpg?itok=6vRJjb8w"],
+    ["euclid", "euclid@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Euklid-von-Alexandria_1.jpg/220px-Euklid-von-Alexandria_1.jpg"],
+    ["lovelace", "alovelace@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/c/c0/Ada_Lovelace_Chalon_portrait.jpg"],
+    ["turing", "aturing@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Alan_Turing_Aged_16.jpg/220px-Alan_Turing_Aged_16.jpg"],
+    ["hilbert", "dhilbert@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Hilbert.jpg/220px-Hilbert.jpg"],
+    ["riemann", "briemann@mathmail.com", "123456789", "https://www.sapaviva.com/wp-content/uploads/2017/06/24S.-Georg-Bernhard-Riemann-1826-1866-420x420.jpg"],
+    ["germain", "sgermain@mathmail.com", "123456789", "https://www.thefamouspeople.com/profiles/images/sophie-germain-1.jpg"],
+    ["fibonacci", "lfibonacci@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/a/a2/Fibonacci.jpg"],
+    ["noether", "enoether@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Noether_retusche_nachcoloriert.jpg/220px-Noether_retusche_nachcoloriert.jpg"],
+    ["wiles", "awiles@mathmail.com", "123456789", "http://famous-mathematicians.org/wp-content/uploads/2013/07/Andrew-Wiles-300x208.jpg"],
+    ["fourier", "jfourier@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Fourier2.jpg/250px-Fourier2.jpg"],
+    ["galois", "egalois@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Evariste_galois.jpg/464px-Evariste_galois.jpg"],
+    ["cantor", "gcantor@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Georg_Cantor2.jpg/220px-Georg_Cantor2.jpg"],
+    ["ramanujan", "sramanujan@mathmail.com", "123456789", "http://trinitycollegechapel.com/media/imagestore/editable-box-images/Ramanujan.jpg"],
+    ["newton", "inewton@mathmail.com", "123456789", "http://www-history.mcs.st-andrews.ac.uk/BigPictures/Newton_2.jpeg"],
+    ["kovalevskaya", "skovalevskaya@mathmail.com", "123456789", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Sofja_Wassiljewna_Kowalewskaja_1.jpg/220px-Sofja_Wassiljewna_Kowalewskaja_1.jpg"],
+    ["cartwright", "mcartwright@mathmail.com", "123456789", "http://www-history.mcs.st-and.ac.uk/BigPictures/Cartwright_4.jpeg"]
 ]
 
-user_list.each do |username, email, pw|
-    User.create(username: username, email: email, password: pw)
+user_list.each do |username, email, pw, avatarUrl|
+    user = User.create(username: username, email: email, password: pw)
+    file = open(avatarUrl)
+    user.avatar.attach(io: file, filename: 'avatar.jpg')
 end
 
 fermats_first_post = "{\"title\":\"\",\"body\":\"It is impossible to separate a cube into two cubes, or a fourth power into two fourth powers, or in general, any power higher than the second, into two like powers. I have discovered a truly marvelous proof of this, which the character limit on this post is too small to contain.\"}";
