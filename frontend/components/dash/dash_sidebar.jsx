c6c4f2e68cb92d45f2746f54c1c5b7b14aa64287
lucas schraier <lucasfschraier@gmail.com>
2019-04-20 19:38:09 -0400

    Add links to personal sites

--- frontend/components/dash/dash_sidebar.jsx
+++ frontend/components/dash/dash_sidebar.jsx
@@ -37,7 +37,7 @@ class DashSidebar extends React.Component {
         const lis = this.state.users.slice(0,4).map( (user) => {
             return(
                 <div key={user.id}>
-                <li>
+                <li className="reclist-li">
                     <div>
                     <img className="avatar-small" src={user.avatar}></img>
                     <span>{user.username}</span>
@@ -54,8 +54,30 @@ class DashSidebar extends React.Component {
         });
         return (
             <div className="dash-sidebar-container">
-                <h3>Recommended Blogs</h3>
-                <ul>
+                <ul className="links-list">
+                    <li className="links-list-li">
+                        <a href="https://www.lschraier.com">
+                            <i className="fas fa-globe"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/lfs48">
+                            <i className="fab fa-github"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/in/lucas-schraier-559baa180/">
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://angel.co/lucas-schraier">
+                            <i className="fab fa-angellist"></i>
+                        </a>
+                    </li>
+                </ul>
+                <h3 className="reclist-header">Recommended Blogs</h3>
+                <ul className="reclist">
                     {lis}
                 </ul>
                 <Link to="/explore" >
