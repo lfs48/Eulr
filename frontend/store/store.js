719fb5a8637e1cd786991d935ba49ea8c32c65b2
lucas schraier <lucasfschraier@gmail.com>
2019-06-28 15:25:49 -0400

    Apply logger middleware only in dev envrionment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,10 +3,17 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
+let middleware;
+if (process.env.NODE_ENV === 'development') {
+    middleware = applyMiddleware(thunk, logger);
+} else {
+     middleware = applyMiddleware(thunk);
+}
+
 const configureStore = (preloadedState = {}) => createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk)
+    middleware
 );
 
 export default configureStore;
