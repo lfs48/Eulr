fb95162a9260fa242504a6d72ae4708dd5bbd329
lucas schraier <lucasfschraier@gmail.com>
2019-06-28 16:36:51 -0400

    Map array of users to props

--- frontend/components/nav/search/search_container.jsx
+++ frontend/components/nav/search/search_container.jsx
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import Search from './search';
 
 const msp = (state) => ({
-
+    users: Object.values(state.entities.users)
 });
 
 const mdp = (dispatch) => ({
