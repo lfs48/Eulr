1479ea961884e5c50a33a47c2a7e7d91237f1717
lucas schraier <lucasfschraier@gmail.com>
2019-07-01 20:16:08 -0400

    Fix syntax errors, add indexes

--- db/migrate/20190701235145_create_messages.rb
+++ db/migrate/20190701235145_create_messages.rb
@@ -1,9 +1,11 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.integer sender_id, null: false
-      t.integer receiver_id, null: false
-      t.text message, null: false
+      t.integer :sender_id, null: false
+      t.integer :receiver_id, null: false
+      t.index :sender_id
+      t.index :receiver_id
+      t.text :body, null: false
       t.timestamps
     end
   end
