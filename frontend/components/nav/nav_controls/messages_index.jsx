2d9ecf0ed674f56249cb5e061893cd79ea149ed3
lucas schraier <lucasfschraier@gmail.com>
2019-07-03 19:53:30 -0400

    Render list of messages when menu is open

--- frontend/components/nav/nav_controls/messages_index.jsx
+++ frontend/components/nav/nav_controls/messages_index.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchMessages } from '../../../actions/entities/message_actions';
+import { fetchUsers } from '../../../actions/entities/user_actions';
 
 const MessagesIndex = props => {
 
@@ -8,10 +9,11 @@ const MessagesIndex = props => {
 
     const [isOpen, switchState] = useState(false);
 
-    const {currentUser, messages} = useSelector(
+    const {currentUser, users, messages} = useSelector(
         state => ({
             currentUser: state.entities.users[state.session.id],
-            messages: state.entities.messages
+            users: state.entities.users,
+            messages: Object.values(state.entities.messages)
         })
     );
 
@@ -22,13 +24,30 @@ const MessagesIndex = props => {
         []
     );
 
-    const lis = {}
+    const lis = messages.map( 
+        message => {
+            const url = users[message.sender_id] ? users[message.sender_id].avatar : "";
+            return(
+            <li key={message.id}>
+                <img className="avatar-small" src={url} />
+                <span>{message.body}</span>
+            </li>
+            )
+        }
+    );
 
     return(
         <div className="messages-index-container">
             <i className="fas fa-comment" onClick={() => switchState(!isOpen)}></i>
             {isOpen? 
             <div className="message-menu-container">
+                <ul>
+                    <li>
+                        <span>{currentUser.username}</span>
+                        <button>New Message</button>
+                    </li>
+                    {lis}
+                </ul>
             </div>
             :<></>}
         </div>
