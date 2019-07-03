4af09e36358a3b5b898bc0f54c5b685141b1b658
lucas schraier <lucasfschraier@gmail.com>
2019-07-03 15:18:52 -0400

    Map redux state to component using hooks

--- frontend/components/nav/nav_controls/messages_index.jsx
+++ frontend/components/nav/nav_controls/messages_index.jsx
@@ -1,14 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { useSelector } from 'react-redux';
+import { fetchMessages } from '../../../actions/entities/message_actions';
 
-function MessagesIndex() {
+const MessagesIndex = props => {
 
     const [isOpen, switchState] = useState(false);
+
+    const {currentUser, messages} = useSelector(
+        state => ({
+            currentUser: state.entities.users[state.session.id],
+            messages: state.entities.messages
+        })
+    );
+
+    useEffect( 
+        () => {
+            fetchMessages(currentUser.id)
+        }
+    );
+
+    const lis = {}
 
     return(
         <div className="messages-index-container">
             <i className="fas fa-comment" onClick={() => switchState(!isOpen)}></i>
             {isOpen? 
-            <div className="message-menu-container"></div>
+            <div className="message-menu-container">
+            </div>
             :<></>}
         </div>
         
