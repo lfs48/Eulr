ff6b1b55c9e5b6e86b321a83b177419dcb0df49a
lucas schraier <lucasfschraier@gmail.com>
2019-07-03 18:50:17 -0400

    Include messages slice of state

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import postsReducer from './posts_reducer';
 import tagsReducer from './tags_reducer';
+import messagesReducer from './messages_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     posts: postsReducer,
-    tags: tagsReducer
+    tags: tagsReducer,
+    messages: messagesReducer
 });
 
 export default entitiesReducer;
