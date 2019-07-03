1b668a9e1b2df6001d03ed7a0e00364c7a4674a9
lucas schraier <lucasfschraier@gmail.com>
2019-07-03 15:28:28 -0400

    Add missing colons to symbols

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,12 @@ class Message < ApplicationRecord
 
     belongs_to :sender,
         class_name: :User,
-        primary_key: id,
+        primary_key: :id,
         foreign_key: :sender_id
 
     belongs_to :receiver,
         class_name: :User,
-        primary_key: id,
+        primary_key: :id,
         foreign_key: :receiver_id
 
 end
