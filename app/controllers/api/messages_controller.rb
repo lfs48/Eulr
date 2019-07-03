8e5c5dc1a1f4973814a306b94b2098810c1b4af0
lucas schraier <lucasfschraier@gmail.com>
2019-07-03 15:35:06 -0400

    Change array concat to array push

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,8 +3,8 @@ class Api::MessagesController < ApplicationController
     def index
         @user = User.find_by(id: params[:user_id])
         @messages = []
-        @messages += Message.find_by(sender_id: @user.id)
-        @messages += Message.find_by(receiver_id: @user.id)
+        @messages << Message.find_by(sender_id: @user.id)
+        @messages << Message.find_by(receiver_id: @user.id)
         render "api/messages/index"
     end
 
