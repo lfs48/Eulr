46136bea36044dcdc5dad056ee28228f81cee828
lucas schraier <lucasfschraier@gmail.com>
2019-07-04 12:52:14 -0400

    Define hideConversation action and corresponding type

--- frontend/actions/ui/conversation_actions.js
+++ frontend/actions/ui/conversation_actions.js
@@ -1,10 +1,16 @@
 export const OPEN_CONVERSATION = "OPEN_CONVERSATION";
+export const HIDE_CONvERSATION = "HIDE_CONVERSATION";
 export const CLOSE_CONVERSATION = "CLOSE_CONVERSATION";
 
 // Standard Actions
 
 export const openConversation = (userId) => ({
     type: OPEN_CONVERSATION,
+    userId: userId
+});
+
+export const hideConversation = (userId) => ({
+    type: HIDE_CONVERSATION,
     userId: userId
 });
 
