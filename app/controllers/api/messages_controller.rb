f3c342417aa9cccc81ec9b3f3af28fa141985063
lucas schraier <lucasfschraier@gmail.com>
2019-07-04 17:12:52 -0400

    Define show action

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -8,4 +8,19 @@ class Api::MessagesController < ApplicationController
         render "api/messages/index"
     end
 
+    def create
+        @message = Message.new(message_params)
+        if @message.save
+            render "api/messages/show"
+        else
+            render @message.errors.full_messages, status: 422
+        end
+    end
+
+    private
+
+    def message_params
+        params.require(:message).permit(:sender_id, :receiver_id, :body)
+    end
+
 end
