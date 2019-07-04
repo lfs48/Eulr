d244170a47ae90ca48fe99abde261d9656303a9e
lucas schraier <lucasfschraier@gmail.com>
2019-07-04 12:32:58 -0400

    Include conversations slice of state

--- frontend/reducers/ui/ui_reducer.js
+++ frontend/reducers/ui/ui_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import modalReducer from './modal_reducer';
 import navReducer from './nav_reducer';
 import postIndexReducer from './post_index_reducer';
+import conversationsReducer from './conversation_reducer';
 
 const uiReducer = combineReducers({
     modal: modalReducer,
     nav: navReducer,
-    postIndex: postIndexReducer
+    postIndex: postIndexReducer,
+    conversations: conversationsReducer
 });
 
 export default uiReducer;
